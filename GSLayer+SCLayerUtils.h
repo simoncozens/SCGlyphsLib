70cea487f7f3f55d648ff96c3894606e7b414c2e
Simon Cozens <simon@simon-cozens.org>
2019-05-23 15:41:52 +0100

    Add a function to show layer coverage.

---
+++ GSLayer+SCLayerUtils.h
@@ -0,0 +1,17 @@
+//
+//  GSLayer+SCLayerUtils.h
+//  SuperTool
+//
+//  Created by Simon Cozens on 04/12/2017.
+//  Copyright © 2017 Simon Cozens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import <GlyphsCore/GlyphsCore.h>
+#import <GlyphsCore/GSLayer.h>
+
+@interface GSLayer (SCLayerUtils)
+-(CGFloat) coverage;
+
+@end
---
+++ GSLayer+SCLayerUtils.m
@@ -0,0 +1,65 @@
+//
+//  GSLayer+SCLayerUtils.m
+//  SuperTool
+//
+//  Created by Simon Cozens on 04/12/2017.
+//  Copyright © 2017 Simon Cozens. All rights reserved.
+//
+
+#import "GSLayer+SCLayerUtils.h"
+
+@implementation GSLayer (SCLayerUtils)
+
+-(CGFloat) coverage {
+    NSBezierPath* p = [self bezierPath];
+    NSInteger black = 0;
+    NSInteger white = 0;
+    NSInteger x;
+    NSInteger y;
+    for (x = 0; x < self.width; x+= self.width/100) {
+        for (y=0; y < self.glyphMetrics.ascender; y+= self.glyphMetrics.ascender/100) {
+            NSPoint pt = NSMakePoint(x,y);
+            if ([p containsPoint:pt]) {
+                black++;
+            } else {
+                white++;
+            }
+        }
+    }
+    return black/(CGFloat)(black+white);
+//    int segments = (int)p.elementCount;
+//    CGFloat white = self.width * (self.glyphMetrics.ascender - self.glyphMetrics.descender);
+//    NSPoint curpoint;
+//    for (int i=0; i<segments; i++) {
+//        NSPoint pointArray[3];
+//        float xa,ya,xb,yb,xc,yc,xd,yd;
+//        NSBezierPathElement e = [p elementAtIndex:i
+//                                 associatedPoints:pointArray];
+//        switch(e) {
+//            case NSMoveToBezierPathElement:
+//                curpoint = pointArray[0];
+//                break;
+//            case NSCurveToBezierPathElement:
+//                xa = curpoint.x; ya = curpoint.y / 20;
+//                xb = pointArray[0].x; yb = pointArray[0].y / 20;
+//                xc = pointArray[1].x; yc = pointArray[1].y / 20;
+//                xd = pointArray[2].x; yd = pointArray[2].y / 20;
+//                black -= (xb-xa)*(10*ya + 6*yb + 3*yc +   yd) + (xc-xb)*( 4*ya + 6*yb + 6*yc +  4*yd) +(xd-xc)*(  ya + 3*yb + 6*yc + 10*yd);
+//                curpoint = pointArray[2];
+//                break;
+//            case NSLineToBezierPathElement:
+//                xa = curpoint.x; ya = curpoint.y / 20;
+//                xb = xa; yb = ya;
+//                xc = pointArray[0].x; yc = pointArray[0].y / 20;
+//                xd = xc; yd = yc;
+//                black -= (xb-xa)*(10*ya + 6*yb + 3*yc +   yd) + (xc-xb)*( 4*ya + 6*yb + 6*yc +  4*yd) +(xd-xc)*(  ya + 3*yb + 6*yc + 10*yd);
+//                curpoint = pointArray[0];
+//                break;
+//
+//            case NSClosePathBezierPathElement:
+//                /* Do nothing */;
+//        }
+//    }
+//    return black/white;
+}
+@end
